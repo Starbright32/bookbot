53b40800a064a8147cb490cb4ae94cf993231815
Starbright32 <meghanflynn2@gmail.com>
2025-11-10 08:58:20 -0500

    Correcting number of words logic

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-"""
-This function takes a filepath as input and 
-returns the contents of the file as a string 
-"""
-def get_book_text(file_path):
+def main():
+  file_path = "../bookbot/books/frankenstein.txt"
+  file_contents = get_book_text(file_path)
+  num_words = number_words(file_contents)
+  print(f"Found {num_words} total words")
 
+def get_book_text(file_path):
   with open(file_path) as f:
     file_contents = f.read()
   return file_contents
 
 def number_words(file_contents):
-
   list_of_words = file_contents.split()
-  num_words = len(list_of_words) 
-
-  print(f"Found {num_words} total words")
-
-"""
-Prints the entire contents of the book to the console 
-"""
-def main():
-  file_path = "../bookbot/books/frankenstein.txt"
-
-  file_contents = get_book_text(file_path)
-  number_words(file_contents)
-  
+  return len(list_of_words)
 
 main()
